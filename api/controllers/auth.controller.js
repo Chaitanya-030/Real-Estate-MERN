4f01e52b38b0b8048fc415c4dfb12b2204d60951
chaitanya <csheth845@gmail.com>
2024-05-27 14:20:46 +0530

    created a middleware and possible error function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,19 +1,18 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10); // it is used to save the hashed password in the database
     // moreover it is already using "sync" in the function itself, so no need to use await for this
-    
+
     const newUser = new User({username, email, password: hashedPassword});
 
     try {
         await newUser.save(); // as this saving takes few seconds, we will use await as to stay in this line until its complete
         res.status(201).json('User created successfully!');
     } catch(err) {
-        res.status(500).json(err.message);
+        next(err);
     }
-    
 };
 
