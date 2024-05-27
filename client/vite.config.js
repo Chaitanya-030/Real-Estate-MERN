f36bb320ad797edbbf0acc4c54dee54779ba1ee2
chaitanya <csheth845@gmail.com>
2024-05-28 02:02:43 +0530

    completed signup functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api' : {
+        target: 'http://localhost:3000', // this helps in not writing this whole thing again and again
+        secure: false,
+      },
+    },
+  },
+
   plugins: [react()],
 })
