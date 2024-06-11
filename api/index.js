359dc1f86aab2763a2fea3dc0122e7791af0b1ac
chaitanya <csheth845@gmail.com>
2024-06-12 00:02:45 +0530

    added pre installation

--- api/index.js
+++ api/index.js
@@ -5,9 +5,13 @@ import cookieParser from 'cookie-parser';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
+import path from 'path';
+
 
 dotenv.config();
 const app = express();
+
+const __dirname = path.resolve();
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log("Connected to MongoDB");
@@ -27,6 +31,12 @@ app.listen(3000, () => {
   app.use('/api/user', userRouter);
   app.use('/api/auth', authRouter);
   app.use('/api/listing', listingRouter);
+
+  app.use(express.static(path.join(__dirname, '/client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  }) 
   
   app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
